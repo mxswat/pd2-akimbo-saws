9332f0c9f674629d773960cb939575b681388c5a
BAX\cacciapaglia <massimocacciapaglia.13@gmail.com>
2018-05-28 15:26:29 +0200

    Try fix after Nepgearsy suggestion

--- Hooks/weaponfactorytweakdata.lua
+++
@@ -1,5 +0,0 @@
-Hooks:PostHook(WeaponFactoryTweakData, "init", "AkimboHeavyCrossbow_TweakDatainit", function(wFac)	
-	for k, v in pairs(wFac.parts or {}) do
-		wFac.parts[k..'_beakimbo'] = deep_clone(v)
-	end
-end)
